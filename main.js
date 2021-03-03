81089c7ef8754567d52d34789ec0b6e1d7b7db87
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-03 13:01:12 -0700

    6 & 8

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-// ##### Introduction #####
-//
-// The below assignment will require NO functions
-//
-// Instead, our focus is going to be on console.logging the characters at certain indices of strings.
-//
+// // ##### Introduction #####
+// //
+// // The below assignment will require NO functions
+// //
+// // Instead, our focus is going to be on console.logging the characters at certain indices of strings.
+// //
 
 
-// ##### Challenges #####
+// // ##### Challenges #####
 
-console.log('##### Challenge 1 #####');
-// 1.
-//
-// Console out the numbers from 1-20, but only if they're divisible by 3.
-// Hint: Use `if` and a modulus operation.
-for (let i = 0; i < 20; i++){
-    if ([i] % 3 === 0){
-    console.log(i);
-}
-}
-
-
-console.log('##### Challenge 2 #####');
-// 2.
-//
-// Console out the numbers from 1-20. BACKWARDS.
-
-for (let i = 20; i > 0; i--) {
-    if([i] <= 20 && [i] >= 0){
-        console.log([i])
-    }
-}
-
-console.log('##### Challenge 3 #####');
-// 3.
-//
-// For the string 'Boy howdy am I good at this!', console out the letters
-// individually.
-let string3 = 'Boy howdy am I good at this!';
-for (i = 0; i < string3.length; i++){
-    console.log(string3[i]);
-}
+// console.log('##### Challenge 1 #####');
+// // 1.
+// //
+// // Console out the numbers from 1-20, but only if they're divisible by 3.
+// // Hint: Use `if` and a modulus operation.
+// for (let i = 0; i < 20; i++){
+//     if ([i] % 3 === 0){
+//     console.log(i);
+// }
+// }
 
 
-console.log('##### Challenge 4 #####');
-// 4.
-//
-// For the string, 'And getting better every day.', starting with the second
-// character, print out every third character.
-let string4 = 'And getting better every day.';
-for (i = 2; i < string4.length; i+=3){
-    // console.log([i]);
-    console.log(string4[i]);
-}
+// console.log('##### Challenge 2 #####');
+// // 2.
+// //
+// // Console out the numbers from 1-20. BACKWARDS.
 
-console.log('##### Challenge 5 #####');
-// 5. 
-//
-// For the string 'Am I the best?', console out the letters individually, AND
-// UPPERCASED.
-let string5 = 'Am I the best?';
-for (i = 0; i < string5.length; i++){
-    console.log(string5[i].toUpperCase());
-}
+// for (let i = 20; i > 0; i--) {
+//     if([i] <= 20 && [i] >= 0){
+//         console.log([i])
+//     }
+// }
+
+// console.log('##### Challenge 3 #####');
+// // 3.
+// //
+// // For the string 'Boy howdy am I good at this!', console out the letters
+// // individually.
+// let string3 = 'Boy howdy am I good at this!';
+// for (i = 0; i < string3.length; i++){
+//     console.log(string3[i]);
+// }
+
+
+// console.log('##### Challenge 4 #####');
+// // 4.
+// //
+// // For the string, 'And getting better every day.', starting with the second
+// // character, print out every third character.
+// let string4 = 'And getting better every day.';
+// for (i = 2; i < string4.length; i+=3){
+//     // console.log([i]);
+//     console.log(string4[i]);
+// }
+
+// console.log('##### Challenge 5 #####');
+// // 5. 
+// //
+// // For the string 'Am I the best?', console out the letters individually, AND
+// // UPPERCASED.
+// let string5 = 'Am I the best?';
+// for (i = 0; i < string5.length; i++){
+//     console.log(string5[i].toUpperCase());
+// }
 
 console.log('##### Challenge 6 #####');
 // 6.
@@ -69,43 +69,54 @@ console.log('##### Challenge 6 #####');
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 let string6 = 'Whoa, I am the best!';
-for (i = 0; i < string6.length; i++){
-    string6.split(" ")
-    console.log(string6[i]);
+let newArr = string6.split(" ");
+console.log(newArr);
+newArr[2] = newArr[2].toUpperCase();
+console.log(newArr);
+let joinArr = newArr.join(" ")
+console.log(joinArr)
+
+for (i = 0; i < joinArr.length; i++){
+    console.log(joinArr[i]);
 }
 
 
-console.log('##### Challenge 7 #####');
-// 7.
-//
-// For the string 'I am become death, destroyer of worlds.', console out the letters
-//  individually, but backwards. That is, the first console log should be
-// `.`, followed by `s`, followed by `d`, and so on.
-let string7 = 'I am become death, destroyer of worlds.';
-for (i = string7.length-1; i > -1; i--){
-    console.log(string7[i]);
-}
+// console.log('##### Challenge 7 #####');
+// // 7.
+// //
+// // For the string 'I am become death, destroyer of worlds.', console out the letters
+// //  individually, but backwards. That is, the first console log should be
+// // `.`, followed by `s`, followed by `d`, and so on.
+// let string7 = 'I am become death, destroyer of worlds.';
+// for (i = string7.length-1; i > -1; i--){
+//     console.log(string7[i]);
+// }
 
 
-console.log('##### Challenge 8 #####');
-// 8.
-//
-const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|Steve H.|Cesar|Nastya|Jeremy L.|Michael|John|Lee|Ruth|Steve M.|Tim|Ronny|Stevie R|Neo|Rick|Kyle|Minahil|Adam|Sonny|James|Daisy|Willie';
+// console.log('##### Challenge 8 #####');
+// // 8.
+// //
+// const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|Steve H.|Cesar|Nastya|Jeremy L.|Michael|John|Lee|Ruth|Steve M.|Tim|Ronny|Stevie R|Neo|Rick|Kyle|Minahil|Adam|Sonny|James|Daisy|Willie';
 
-// Console out the individual letters of each person in class, which are
-// in the string above. Every time you start a new name (including the first
-// one!), console out the string: 'Here today is:'.
+// // Console out the individual letters of each person in class, which are
+// // in the string above. Every time you start a new name (including the first
+// // one!), console out the string: 'Here today is:'.
 
-// You'll know you're at a new person's name when you hit the pipe character.
+// // You'll know you're at a new person's name when you hit the pipe character.
 
-// Don't print the pipes!
+// // Don't print the pipes!
 
-for (const value of names){
-    if (value === "|"){
-        console.log("Here today is: ")
-    } else {
-        console.log(value)
-    }
-}
-
+// console.log("Here today is: ")
+// for (const value of names){
+//     if (value === "|"){
+//         console.log("Here today is: ")
+//     } else {
+//         console.log(value)
+//     }
+// }
+// // for (i = 0; i < names.length; i++){
+// //     if (i === 0){
+// //         console.log("Here today is: ")
+// //     }
+// // }
 
